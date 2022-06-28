9a71e99030b9ef35cf2c5d32ed4bd41733a4f54d
HiddenUniverse <108257107+HiddenUniverse@users.noreply.github.com>
2022-06-28 19:39:45 +0300

    v1.0.2.5

--- AutoBuffTimer.cs
+++ AutoBuffTimer.cs
@@ -9,8 +9,16 @@ namespace HiddenUniverse_WebClient
     internal class AutoBuffTimer
     {
         private System.Windows.Forms.Timer timer { get; set; }
+        private System.Windows.Forms.Timer countdownTimer { get; set; }
         public System.Windows.Forms.Timer Timer { get { return timer; } }
+        public System.Windows.Forms.Timer CountdownTimer { get { return countdownTimer; } }
+        private System.Windows.Forms.Timer threadTimer { get; set; }
+        public System.Windows.Forms.Timer ThreadTimer { get { return threadTimer; } }
+
         public int autoBuffInterval = 0;
+        public float cdelta = 0;
+        public int lastTimerIInterval;
+        
         public void InitTimer()
         {
             if (autoBuffInterval > 0)
@@ -24,6 +32,49 @@ namespace HiddenUniverse_WebClient
         private void Timer_Tick(object sender, EventArgs e)
         {
             FlyffWCForm.Instance.initiateBuff();
+        }
+
+        public void InitCountdown()
+        {
+            countdownTimer = new System.Windows.Forms.Timer();
+            countdownTimer.Tick += new EventHandler(CountdownTimer_Tick);
+            countdownTimer.Interval = 100;
+            countdownTimer.Start();
+        }
+        private void CountdownTimer_Tick(object sender, EventArgs e)
+        {
+            if (timer == null || !timer.Enabled )
+            {
+                FlyffWCForm.Instance.autoBuffText.Enabled = false;
+                FlyffWCForm.Instance.autoBuffText.Visible = false;
+                countdownTimer.Stop();
+                return;
+            }
+            if (lastTimerIInterval != autoBuffInterval || cdelta == autoBuffInterval)
+            {
+                cdelta = 0;
+                lastTimerIInterval = autoBuffInterval;
+            }
+            cdelta = cdelta + countdownTimer.Interval;
+            string cdsec = ((timer.Interval - cdelta) / 1000).ToString("0.0");
+            FlyffWCForm.Instance.autoBuffText.Text = cdsec + " Seconds";
+        }
+        public void InitThreadTimer()
+        {
+            threadTimer = new System.Windows.Forms.Timer();
+            threadTimer.Tick += new EventHandler(ThreadTimer_Tick);
+            threadTimer.Interval = 1000; // in miliseconds
+            threadTimer.Start();
+        }
+        private void ThreadTimer_Tick(object sender, EventArgs e)
+        {
+            if (!FlyffWCForm.Instance.buffThread.IsAlive && FlyffWCForm.Instance.healWasEnabled)
+            {
+                FlyffWCForm.Instance.autoHealBox.Checked = true;
+                threadTimer.Stop();
+                return;
+            }
+            else if (!FlyffWCForm.Instance.buffThread.IsAlive) { threadTimer.Stop(); }
         }
     }
 }
